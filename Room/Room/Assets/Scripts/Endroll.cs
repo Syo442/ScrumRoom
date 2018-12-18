e241c683fda1f978dc69ca9b0f6e44b19caa8dc7
Syo442 <syo0618.jm@gmail.com>
2018-12-19 02:38:07 +0900

    BGM追加

--- Room/Room/Assets/Scripts/Endroll.cs
+++ Room/Room/Assets/Scripts/Endroll.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using DG.Tweening;
 
 public class Endroll : MonoBehaviour {
 
 	[SerializeField]
 	RectTransform text;
-	
+	[SerializeField]
+	GameObject videoPlayer;
 	void Start () {
-			text.DOLocalMoveY(1500.0f, 42.0f).SetRelative().SetEase(Ease.Linear);
+			text.DOLocalMoveY(1560.0f, 42.0f).SetRelative().SetEase(Ease.Linear).OnComplete(() => {
+				videoPlayer.GetComponent<RawImage>().DOFade(0.0f,0.5f);
+			});
 	}
 }
